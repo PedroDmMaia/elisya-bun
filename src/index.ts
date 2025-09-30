7611ea6c58e4928e9c1f6a1b903596d17879634d
Pedro <pedrodelmonico@hotmail.com>
2025-09-29 23:43:40 -0300

    feat: add PostgreSQL database setup and authentication schema

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Elysia } from "elysia";
 import { openapi } from '@elysiajs/openapi'
 import { z } from 'zod'
+import { betterAuthPlugin, OpenAPI } from "./http/plugins/better-auth";
 
 const app = new Elysia()
-  .use(openapi())
+  .use(openapi({
+    documentation: {
+      components: await OpenAPI.components,
+      paths: await OpenAPI.getPaths()
+    }
+  }))
+  .use(betterAuthPlugin)
   .get("/", () => "Hello Elysia")
   .get('user/:id', ({ params }) => {
     const { id } = params
